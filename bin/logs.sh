0ec257c049ecb682729b99b2b2ce553e68f42bb6
Marcos Ortiz <ormarcos@amazon.com>
2024-06-01 14:09:02 +0000

    First commit.

---
+++ bin/logs.sh
@@ -0,0 +1,5 @@
+sam logs \
+    --region us-east-1 \
+    --stack-name sns-sqs \
+    --name QueueConsumerFunction \
+    -t
---
+++ bin/sqs-rcv-msg.sh
@@ -0,0 +1,3 @@
+aws sqs receive-message \
+    --region us-east-1 \
+    --queue-url https://sqs.us-east-1.amazonaws.com/564151284033/sns-sqs-MySqsQueue-IJ0r1x66Q5ud
