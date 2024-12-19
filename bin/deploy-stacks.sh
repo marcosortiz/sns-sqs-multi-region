675e221dd08251a99c0fb05abe107be9148a551e
Marcos Ortiz <ormarcos@amazon.com>
2024-12-19 14:59:54 +0000

    Uncommenting sam deploy lines.

--- bin/deploy-stacks.sh
+++ bin/deploy-stacks.sh
@@ -3,12 +3,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR/config.sh"
 
 
-# echo "Deploying the stack on ${PRIMARY_REGION} ..."
-# sam deploy --stack-name $STACK_NAME --region $PRIMARY_REGION --config-env $PRIMARY_ENV --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --resolve-s3
+echo "Deploying the stack on ${PRIMARY_REGION} ..."
+sam deploy --stack-name $STACK_NAME --region $PRIMARY_REGION --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --resolve-s3
 aws cloudformation describe-stacks --stack-name $STACK_NAME --region $PRIMARY_REGION --query 'Stacks[0].Outputs' --output json > config/$PRIMARY_ENV.json
 
-# echo "Deploying the stack on ${SECONDARY_REGION} ..."
-# sam deploy --stack-name $STACK_NAME --region $SECONDARY_REGION --config-env $SECONDARY_ENV --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --resolve-s3
+echo "Deploying the stack on ${SECONDARY_REGION} ..."
+sam deploy --stack-name $STACK_NAME --region $SECONDARY_REGION --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --resolve-s3
 aws cloudformation describe-stacks --stack-name $STACK_NAME --region $SECONDARY_REGION --query 'Stacks[0].Outputs' --output json > config/$SECONDARY_ENV.json
 
 # Combine the outputs into a single JSON file
